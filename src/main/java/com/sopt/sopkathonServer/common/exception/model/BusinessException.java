c50369c56086b21cbe67eeffc74c358e24469f48
ddongseop <dlehdtjq00o@gmail.com>
2023-11-24 00:29:29 +0900

    [CHORE] #1 common 클래스 추가

---
+++ src/main/java/com/sopt/sopkathonServer/common/exception/model/BusinessException.java
@@ -0,0 +1,19 @@
+package com.sopt.sopkathonServer.common.exception.model;
+
+import com.sopt.sopkathonServer.common.exception.enums.ErrorType;
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private final ErrorType errorType;
+
+    public BusinessException(ErrorType errorType) {
+        super(errorType.getMessage());
+        this.errorType = errorType;
+    }
+
+    public int getHttpStatus() {
+        return errorType.getHttpStatusCode();
+    }
+}
