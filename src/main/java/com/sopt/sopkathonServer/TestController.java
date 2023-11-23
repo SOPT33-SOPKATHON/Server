60dfdefe10fa5dbe922f3dc7c323cf4d597c4364
ddongseop <dlehdtjq00o@gmail.com>
2023-11-23 18:03:38 +0900

    초기 세팅

---
+++ src/main/java/com/sopt/sopkathonServer/TestController.java
@@ -0,0 +1,21 @@
+package com.sopt.sopkathonServer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String test() {
+        log.info("[test] API 실행");
+        return "테스트 성공";
+    }
+}
---
+++ src/test/java/com/sopt/sopkathonServer/SopkathonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopt.sopkathonServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopkathonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
