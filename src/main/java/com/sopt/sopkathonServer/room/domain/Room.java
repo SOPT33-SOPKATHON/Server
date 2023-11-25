cee16fc5181b9e11b127570e9563f6a8a9209e2d
ddongseop <dlehdtjq00o@gmail.com>
2023-11-25 22:59:19 +0900

    [FEAT] entity 정의

---
+++ src/main/java/com/sopt/sopkathonServer/room/domain/Room.java
@@ -0,0 +1,51 @@
+package com.sopt.sopkathonServer.room.domain;
+
+import com.sopt.sopkathonServer.celeb.domain.Celeb;
+import com.sopt.sopkathonServer.common.domain.BaseTimeEntity;
+import com.sopt.sopkathonServer.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Table(name = "room")
+public class Room extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "room_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String roomName;
+
+    private String roomContent;
+
+    private int year;
+    private int month;
+    private int day;
+
+    @Column(nullable = false)
+    private String roomUUID;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @OneToMany(mappedBy = "room")
+    private List<Celeb> celebList = new ArrayList<>();
+
+    public void addCelebList(Celeb celeb) {
+        celebList.add(celeb);
+    }
+}
