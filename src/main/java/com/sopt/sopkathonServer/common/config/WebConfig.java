ae3c10cc527dd6b8c4b4800a670e6585b88310e0
ddongseop <dlehdtjq00o@gmail.com>
2023-12-14 15:27:06 +0900

    [ADD] 최종 배포 URL

--- src/main/java/com/sopt/sopkathonServer/common/config/WebConfig.java
+++ src/main/java/com/sopt/sopkathonServer/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:8080", "http://192.168.219.154:5173")
+                .allowedOrigins("http://localhost:5173", "http://localhost:8080", "http://192.168.219.154:5173", "https://cong-ratulation.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .maxAge(3000);
